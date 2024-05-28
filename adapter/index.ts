0b279ee978eb6e660d2929189b3bfb1bf0145ba2
HifiAudi0 <77525153+HifiAudi0@users.noreply.github.com>
2024-05-28 00:03:15 +0000

    Installed and setup vitest running okay on sample code.

--- adapter/index.ts
+++ adapter/index.ts
@@ -1,4 +1,4 @@
-import assignment from "./assignment-3";
+import assignment from "./assignment-4";
 //import assignment from "./assignment-1";
 // Whatever assignments gets commented out here won't be run by mcmasterful-books
 
